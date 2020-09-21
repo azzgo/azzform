0eb73ce499963991999b7c2d8e4874b9f354de93
Yishan Wen <wittywys@gmail.com>
2020-09-21 21:52:41 +0800

    feat: add param description

--- src/plugins/builder/index.js
+++ src/plugins/builder/index.js
@@ -1,9 +1,14 @@
 import FormBuilder from "./builder.vue";
 import { Modal, Form } from 'ant-design-vue'
 
+/**
+ * 
+ * @param {Vue} Vue Vue 实例
+ */
 FormBuilder.install = function(Vue) {
   Vue.use(Form);
-  Vue.use(Modal)
+  Vue.use(Modal);
+
   Vue.component("form-builder", FormBuilder);
 };
 
