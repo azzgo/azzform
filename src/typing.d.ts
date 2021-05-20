79e91e5d7387b7454d118856a123d444fdbdde2c
Yishan Wen <wittywys@gmail.com>
2021-05-20 20:28:01 +0800

    chore: delete vue decleare for there exist one

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -2,8 +2,3 @@ declare interface ChangeEvent<T = HTMLInputElement> {
   target: T;
 }
 
-declare module '*.vue' {
-  import { Component } from 'vue'
-  const component: Component
-  export default component
-}
