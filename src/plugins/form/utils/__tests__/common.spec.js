77ea6e29a3a3bd14d64b57249719f3a843e6f036
Yishan Wen <wittywys@gmail.com>
2021-05-23 00:33:47 +0800

    feat: add fieldData path  to schemaPath conventor

---
+++ src/plugins/form/utils/__tests__/common.spec.js
@@ -0,0 +1,28 @@
+import { get } from "lodash";
+import { getTargetSchemaPath } from "../common";
+
+test("give a data path, i can get target value json schema definition", () => {
+  const dataPath = "container.name";
+  const schema = {
+    properties: {
+      container: {
+        properties: {
+          name: {
+            type: "string",
+            title: "name",
+            description: "your name",
+          },
+        },
+      },
+    },
+  };
+
+  const schemaPath = getTargetSchemaPath(dataPath);
+  console.log(schemaPath);
+
+  expect(get(schema, schemaPath)).toEqual({
+    type: "string",
+    title: "name",
+    description: "your name",
+  });
+});
--- src/plugins/form/utils/common.ts
+++ src/plugins/form/utils/common.ts
@@ -8,3 +8,7 @@ export function checkIsWidget(schema: Record<string, any>): boolean {
   return schema.widget != null
 }
 
+export function getTargetSchemaPath(formDataPath: string): string {
+  return formDataPath?.split(".").map(str => `properties.${str}`).join(".")
+}
+
